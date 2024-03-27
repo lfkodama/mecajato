274f8877062c9c13ff64371e411a93257d3c65e3
Fernando Kodama <lfkodama@gmail.com>
2024-03-27 20:55:55 -0300

    Update clients form show

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -3,3 +3,19 @@ function add_carro() {
     html = "<br>  <div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name='carro' > </div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control' name='placa' ></div> <div class='col-md'> <input type='number' placeholder='ano' class='form-control' name='ano'> </div> </div>";
     container.innerHTML += html;
 }
+
+function exibir_form(tipo){
+
+    add_cliente = document.getElementById('adicionar-cliente')
+    att_cliente = document.getElementById('att_cliente')
+
+    if(tipo == "1"){
+        att_cliente.style.display = "none"
+        add_cliente.style.display = "block"
+
+    }else if(tipo == "2"){
+        add_cliente.style.display = "none";
+        att_cliente.style.display = "block"
+    }
+
+}
