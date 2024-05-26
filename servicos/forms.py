3de3d157c2314a3f640e371a8eae1cde1aca69c6
Fernando Kodama <lfkodama@gmail.com>
2024-05-26 11:09:45 -0300

    Writing Service Order to DB

--- servicos/forms.py
+++ servicos/forms.py
@@ -12,3 +12,10 @@ class FormServico(ModelForm):
             self.fields[field].widget.attrs.update({'class': 'form-control'})
             self.fields[field].widget.attrs.update({'placeholder': field})
             
+        choices = list()
+        
+        for i, j in self.fields['categoria_manutencao'].choices:
+            categoria = CategoriaManutencao.objects.get(titulo = j)
+            choices.append((i.value, categoria.get_titulo_display()))
+            
+        self.fields['categoria_manutencao'].choices = choices
